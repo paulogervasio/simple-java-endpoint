c861c70f1d2367c5b9b6e33f5922a1f28d6cfa5b
Your Name <you@example.com>
2018-08-19 03:31:24 -0300

    .

--- src/main/Application.java
+++ src/main/Application.java
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan("config")
 @SpringBootApplication
-public class Application {
+public class Application { 
 
 	public static void main(String[] args) {
 
