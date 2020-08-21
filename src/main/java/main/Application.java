32a7c5ee7785f037f944150d4decdea24cca2800
Paulo Gervasio <pgervasiosousa@gmail.com>
2020-08-21 13:25:20 -0300

    refactoring main class

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -1,43 +1,17 @@
 package main;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@ComponentScan("config")
 @SpringBootApplication
+@ComponentScan("config")
+@Configuration
 public class Application {
 
 	public static void main(String[] args) {
-
-		Map<String, Object> mapProp = new HashMap<String, Object>();
-		mapProp.put("server.port", "8081");
-
-		SpringApplication app = new SpringApplication(Application.class);
-		app.setDefaultProperties(mapProp);
-		app.run(args);
-
+		SpringApplication.run(Application.class, args);
 	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-		return args -> {
-
-			System.out.println("Here you can inspect the beans provided by Spring Boot:");
-
-			String[] beanNames = ctx.getBeanDefinitionNames();
-			Arrays.sort(beanNames);
-			/*
-			 * for (String beanName : beanNames) { System.out.println(beanName); }
-			 */
-		};
-	}
-
 }
