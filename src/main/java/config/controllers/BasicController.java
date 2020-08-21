e1ec547310c867ccc7d47f35f5bd92e18306edc0
Paulo Gervasio <pgervasiosousa@gmail.com>
2020-08-21 13:25:45 -0300

    refactoring initial controller class

--- src/main/java/config/controllers/BasicController.java
+++ src/main/java/config/controllers/BasicController.java
@@ -2,6 +2,7 @@ package config.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+
 import java.net.InetAddress;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,18 +10,31 @@ import org.springframework.core.env.Environment;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
 @RestController
 public class BasicController { 
+	
+	
+    private static final Logger logger = LogManager.getLogger(BasicController.class);
+
 	
 	@Autowired
 	Environment environment;
 
 	@RequestMapping(value="/", produces=MediaType.APPLICATION_JSON_VALUE)
 	public String index() {
+		
+        logger.info("Info log");
 
-		String msg = "Spring Boot";
-		msg += InetAddress.getLoopbackAddress().getHostAddress()  + "-";
-		msg += InetAddress.getLoopbackAddress().getHostName()  + "----------";
+
+		String msg = "Spring Boot\n";
+		msg += InetAddress.getLoopbackAddress().getHostAddress()  + "\n";
+		msg += InetAddress.getLoopbackAddress().getHostName()  + "\n";
+		msg += System.currentTimeMillis() + "\n";
 		
 		msg += System.getenv("HOSTNAME");
 		
